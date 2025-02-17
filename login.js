040fb4f49a513402ffb191d5b220fffb2c6a909d
amisharn <niroulaamisharaj@gmail.com>
2025-02-17 22:46:53 +0545

    first commit

---
+++ login.js
@@ -0,0 +1,44 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCTXjg7j6qMeWfeHVyDC5oI-Fkr6v0qf6E",
+  authDomain: "expensetracker-691fa.firebaseapp.com",
+  projectId: "expensetracker-691fa",
+  storageBucket: "expensetracker-691fa.firebasestorage.app",
+  messagingSenderId: "1052498168347",
+  appId: "1:1052498168347:web:5c9955e345990cbbfa86be",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const signin = document.getElementById("signin");
+
+signin.addEventListener("click", (e) => {
+  const user = document.getElementById("username").value;
+  const pw = document.getElementById("password").value;
+  e.preventDefault();
+  const auth = getAuth();
+  signInWithEmailAndPassword(auth, user, pw)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      alert("Logging In");
+      window.location.href = "homepage.html";
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert("Invalid username or password");
+      // ..
+    });
+});
